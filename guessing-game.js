8387690ba9f6b9a4781dc38818d13a62cd00c0f1
Eleanor Billerbeck <ellie.billerbeck@gmail.com>
2021-12-29 18:40:02 -0600

    Range

--- guessing-game.js
+++ guessing-game.js
@@ -4,9 +4,13 @@ const { stdin: input, stdout: output } = require('process');
 
 const rl = readline.createInterface({ input, output });
 
+function randomInRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // Minimum and Maximum are both inclusive
+}
 
-
-let secretNumber = 5;
+let secretNumber = randomInRange(0, 100);
 
 function checkGuess(guess){
     let number = Number(guess);
@@ -42,7 +46,14 @@ function askGuess(){
     });
 }
 
-askGuess();
+const maxAnswer = (answer) => {
+    console.log("Enter a max number: ");
+}
+function askRange() {
+    rl.question("Enter a min number: ", maxAnswer);
+}
+
+//askGuess();
 
 //do not need to console.log() askGuess
 //becuase it does not have a return value (the console.log gives us undefined)
